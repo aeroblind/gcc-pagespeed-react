6c6d30b6c5e2bf74aa785af7313a7ebe9c24aef1
Mark Eaton <marke@nobrainer2.com>
2019-05-30 10:15:09 -0500

    fixed typo in expressJs code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('/dist/index.html'));
+  res.sendFile(path.resolve('dist/index.html'));
 });
 
 
